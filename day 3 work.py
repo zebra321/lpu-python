c60020d56ee1056a876364dcc34871f7d9caec88
zebra321 <rakeshmohan6903@gmail.com>
2018-06-08 22:35:52 +0530

    day 3 work.py

---
+++ day 3 work.py
@@ -0,0 +1,6 @@
+print("please type Enter For Every line")
+print("Name __","last_name__","registration_no__","mobile__")
+def getUserData(name,last_name,registration_no,mobile):
+    print("name :",name,"last_name :",last_name,"registration_no :",registration_no,"mobile :",mobile)
+getUserData(input(),input(),input(),int(input()))
+
